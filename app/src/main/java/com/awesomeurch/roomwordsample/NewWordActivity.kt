1221891cf1e24a25e0b7263c2d7355507f3486b2
Nnabugwu Uchechukwu Michael <32470486+awesome-urch@users.noreply.github.com>
2020-05-27 18:20:51 +0100

    Activity and Fragment connected to the data

---
+++ app/src/main/java/com/awesomeurch/roomwordsample/NewWordActivity.kt
@@ -0,0 +1,36 @@
+package com.awesomeurch.roomwordsample
+
+import android.app.Activity
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.TextUtils
+import kotlinx.android.synthetic.main.activity_new_word.*
+
+class NewWordActivity : AppCompatActivity() {
+
+    //private lateinit var editWordView: EditText
+
+    public override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new_word)
+        //editWordView = findViewById(R.id.edit_word)
+
+        //val button = findViewById<Button>(R.id.button_save)
+        button_save.setOnClickListener {
+            val replyIntent = Intent()
+            if (TextUtils.isEmpty(edit_word.text)) {
+                setResult(Activity.RESULT_CANCELED, replyIntent)
+            } else {
+                val word = edit_word.text.toString()
+                replyIntent.putExtra(EXTRA_REPLY, word)
+                setResult(Activity.RESULT_OK, replyIntent)
+            }
+            finish()
+        }
+    }
+
+    companion object {
+        const val EXTRA_REPLY = "com.example.android.wordlistsql.REPLY"
+    }
+}
